f8750a3742727e95c4c98c60b2778704ae053c93
David Pochet <david.pochet@gmail.com>
2022-09-15 07:20:55 +0000

    Add tests on Windows in Github actions -- see dynod/nmk#85
    * also enable pypi publish step only if project builds a python package
    * fix for empty files archive: upgrade to upload-artifacts 2.3.1
    * archive nmk logs as well
    * move python setup items to nmk.yml project file

--- src/tests/test_action.py
+++ src/tests/test_action.py
@@ -11,6 +11,16 @@ class TestGithubPlugin(NmkBaseTester):
     def test_version(self):
         self.nmk(self.prepare_project("ref_github.yml"), extra_args=["version"])
 
-    def test_action(self):
+    def test_action_no_python(self):
         self.nmk(self.prepare_project("ref_github.yml"), extra_args=["gh.actions"])
-        assert (self.test_folder / ".github" / "workflows" / "build.yml").is_file()
+        build_file = self.test_folder / ".github" / "workflows" / "build.yml"
+        assert build_file.is_file()
+        with build_file.open() as f:
+            assert "# Publish to Pypi" not in f.read()
+
+    def test_action_with_python(self):
+        self.nmk(self.prepare_project("ref_github_python.yml"), extra_args=["gh.actions"])
+        build_file = self.test_folder / ".github" / "workflows" / "build.yml"
+        assert build_file.is_file()
+        with build_file.open() as f:
+            assert "# Publish to Pypi" in f.read()
