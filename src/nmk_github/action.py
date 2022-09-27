518b2e96c674a569a2c382688d7b44a1e298ad90
David Pochet <david.pochet@gmail.com>
2022-09-27 11:51:11 +0000

    Github action without loadme.bat -- part of dynod/nmk#109

--- src/nmk_github/action.py
+++ src/nmk_github/action.py
@@ -1,10 +1,10 @@
-from typing import Dict, List
+from typing import List
 
 from nmk_base.common import TemplateBuilder
 
 
 class ActionFileBuilder(TemplateBuilder):
-    def build(self, python_versions: List[str], commands: Dict[str, str]):
+    def build(self, python_versions: List[str], command: str, images: List[str]):
         # Verify if current project is building a python package
         var_name = "pythonPackage"
         has_python_package = var_name in self.model.config and len(self.model.config[var_name].resolve())
@@ -12,5 +12,5 @@ class ActionFileBuilder(TemplateBuilder):
         # Create directory and build from template
         self.main_output.parent.mkdir(parents=True, exist_ok=True)
         self.build_from_template(
-            self.main_input, self.main_output, {"pythonVersions": python_versions, "commands": commands, "hasPythonPackage": has_python_package}
+            self.main_input, self.main_output, {"pythonVersions": python_versions, "command": command, "images": images, "hasPythonPackage": has_python_package}
         )
