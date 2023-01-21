f6bc5c5adbab9bcc5c8f101c69d7026a5c3eabda
David Pochet <david.pochet@gmail.com>
2023-01-21 16:34:38 +0100

    Allow contribution to build/publish steps -- Resolves dynod/nmk#141

--- src/nmk_github/action.py
+++ src/nmk_github/action.py
@@ -1,22 +1,39 @@
-from pathlib import Path
-from typing import List
+from typing import Dict, List
 
-from nmk.model.keys import NmkRootConfig
 from nmk.model.resolver import NmkListConfigResolver
+from nmk.utils import is_condition_set
 from nmk_base.common import TemplateBuilder
+
+# Conditional steps keys
+KEY_IF = "__if__"
+KEY_UNLESS = "__unless__"
+KEY_NAME = "name"
 
 
 class ActionFileBuilder(TemplateBuilder):
-    def build(self, python_versions: List[str], command: str, images: List[str]):
-        # Verify if current project is building a python package
-        var_name = "pythonPackage"
-        has_python_package = var_name in self.model.config and len(self.model.config[var_name].resolve())
-        coverage_report = (
-            Path(self.model.config["pythonCoverageXmlReport"].value).relative_to(self.model.config[NmkRootConfig.PROJECT_DIR].value)
-            if has_python_package
-            else ""
-        )
+    def filter_steps(self, steps: List[Dict[str, str]]) -> List[Dict[str, str]]:
+        # Browse steps
+        out = []
+        for step in steps:
+            added_step = dict(step)
 
+            # Check conditions
+            ok_to_add = True
+            for key, expected in [(KEY_IF, True), (KEY_UNLESS, False)]:
+                if key in added_step:
+                    if is_condition_set(added_step[key]) != expected:
+                        self.logger.debug(f"'{key}' condition not met for build step '{added_step[KEY_NAME]}': {added_step[key]}")  # NOQA: B028
+                        ok_to_add = False
+                        break
+                    del added_step[key]
+
+            # No filter, add it
+            if ok_to_add:
+                out.append(added_step)
+
+        return out
+
+    def build(self, python_versions: List[str], command: str, images: List[str], build_steps: List[Dict[str, str]], publish_steps: List[Dict[str, str]]):
         # Create directory and build from template
         self.main_output.parent.mkdir(parents=True, exist_ok=True)
         self.build_from_template(
@@ -26,8 +43,8 @@ class ActionFileBuilder(TemplateBuilder):
                 "pythonVersions": python_versions,
                 "command": command,
                 "images": images,
-                "hasPythonPackage": has_python_package,
-                "coverageReport": coverage_report,
+                "buildExtraSteps": self.filter_steps(build_steps),
+                "publishExtraSteps": self.filter_steps(publish_steps),
             },
         )
 
