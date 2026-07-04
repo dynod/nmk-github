50a5317c40cc0947cd3c925d2ba140ed100a393e
David Pochet <david.pochet@gmail.com>
2026-07-04 16:30:15 +0200

    Fix git handling

--- src/tests/test_template.py
+++ src/tests/test_template.py
@@ -1,3 +1,6 @@
+import subprocess
+from typing import Any
+
 import pytest
 from buildenv.__main__ import buildenv
 from pytest_multilog import TestHelper
@@ -7,6 +10,16 @@ class TestTemplate(TestHelper):
     def test_template(self, monkeypatch: pytest.MonkeyPatch):
         # Fake a "no CI" environment
         monkeypatch.delenv("CI", raising=False)
+
+        # Fake subprocess to catch git execution
+        old_sub_process_run = subprocess.run
+
+        def filter_git_command(args: list[str], *pargs: list[Any], **kwargs: dict[str, Any]) -> subprocess.CompletedProcess[str]:
+            if args[0] == "git":
+                return subprocess.CompletedProcess(args, 0, str(self.test_folder), "")
+            return old_sub_process_run(args, *pargs, **kwargs)  # type: ignore
+
+        monkeypatch.setattr(subprocess, "run", filter_git_command)
 
         # Prepare a project with uvx backend
         assert (
