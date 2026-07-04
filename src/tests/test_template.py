5a7375bbe68e7831dfe2e59924972c2e26eb7b38
David Pochet <david.pochet@gmail.com>
2026-07-04 16:17:09 +0200

    Final updates

---
+++ src/tests/test_template.py
@@ -0,0 +1,24 @@
+from buildenv.__main__ import buildenv
+from pytest_multilog import TestHelper
+
+
+class TestTemplate(TestHelper):
+    def test_template(self):
+        # Prepare a project with uvx backend
+        assert (
+            buildenv(
+                [
+                    "install",
+                    "--template",
+                    "nmk.python",
+                    "--extra-template",
+                    "nmk.github",
+                    "-p",
+                    str(self.test_folder),
+                ]
+            )
+            == 0
+        )
+
+        # Check github actions file is generated
+        assert (self.test_folder / ".github" / "workflows" / "build.yml").is_file()
